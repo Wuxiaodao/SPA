8d4496eae714fded1dc0644b7c189d9fbd524dc3
吴晓岛 <33849414+Wuxiaodao@users.noreply.github.com>
2020-04-09 00:07:14 +0800

    new update

--- 31-echart/main.js
+++ 31-echart/main.js
@@ -1,29 +1,43 @@
-//所有jQ语法都是在页面完成加载之后完成的
-
-//还有舍入误差问题
 $(function(){
-  var myChart = echarts.init(document.getElementById('main'));
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'));
+    var xData = [];
+        yData = [];
+    for(var p = 0;p<=1;p+=0.1){
+        xData.push(roundFractional(p,1));
+        if(p===0 || p===1){
+            yData.push(0)
+        }else{
+            yData.push(roundFractional(-1*p*Math.log2(p)-(1-p)*Math.log2(1-p),1))
+        }
+        
+    }
+        
+    // 指定图表的配置项和数据
+    var option = {
+        title: {
+            text: '二进制熵函数曲线'
+        },
+        tooltip: {},
+        legend: {
+            data:['信息量']
+        },
+        xAxis: {
+            data: xData
+        },
+        yAxis: {},
+        series: [{
+            name: '信息量',
+            type: 'line',
+            smooth:true,
+            data: yData
+        }]
+    };
 
-  // 指定图表的配置项和数据
-  var option = {
-    title: {
-      text: 'ECharts 入门示例'
-    },
-    tooltip: {},
-    legend: {
-      data:['销量']
-    },
-    xAxis: {
-      data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-    },
-    yAxis: {},
-    series: [{
-      name: '销量',
-      type: 'bar',
-      data: [5, 20, 36, 10, 10, 20]
-    }]
-  };
-
-  // 使用刚指定的配置项和数据显示图表。
-  myChart.setOption(option);    
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+    //小数点后保留第n位
+    function roundFractional(x, n) {
+        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+    }
 });
